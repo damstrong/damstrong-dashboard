f594e29e3196118254f7fe1ff26236217d04d35f
damstrong <evan@damstrong.net>
2025-12-29 14:25:24 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabase = await createSupabaseServerClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
