38e1825c23a23062dd1fdd3e7c0cea5e72f9e38c
damstrong <evan@damstrong.net>
2025-12-29 14:18:51 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { createServerClient } from "@supabase/ssr";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
-  const supabase = createServerClient(
+  const supabase = await createSupabaseServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
