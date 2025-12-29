7880352da6ac463bba30441270f1283cce38f510
damstrong <evan@damstrong.net>
2025-12-29 18:12:53 -0500

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,56 +1,7 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-
-import L from "leaflet";
-const icon = L.icon({
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  iconAnchor: [12, 41],
-});
-(L.Marker.prototype as any).options.icon = icon;
-
-type Point = {
-  lat: number;
-  lng: number;
-  city?: string | null;
-  region?: string | null;
-  country?: string | null;
-  count: number;
-};
+const MapClient = dynamic(() => import("./MapClient"), { ssr: false });
 
 export default function MapPage() {
-  const [points, setPoints] = useState<Point[]>([]);
-
-  useEffect(() => {
-    fetch("/api/map/points")
-      .then((r) => r.json())
-      .then(setPoints)
-      .catch(() => setPoints([]));
-  }, []);
-
-  return (
-    <div style={{ height: "90vh" }}>
-      <div style={{ padding: 12, fontWeight: 700 }}>Visitor Map</div>
-
-      <MapContainer center={[39, -98]} zoom={4} style={{ height: "calc(90vh - 48px)", width: "100%" }}>
-        <TileLayer
-          attribution="© OpenStreetMap"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {points.map((p, i) => (
-          <Marker key={i} position={[p.lat, p.lng]}>
-            <Popup>
-              <b>{[p.city, p.region].filter(Boolean).join(", ")} {p.country ? `(${p.country})` : ""}</b>
-              <br />
-              Visits: {p.count}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </div>
-  );
+  return <MapClient />;
 }
